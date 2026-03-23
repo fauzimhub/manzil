3e5f4f34c5b6d2207251d7460a3db0db9d7045dc
fauzimhub <267496089+fauzimhub@users.noreply.github.com>
2026-03-23 21:55:39 +0700

    Feat: Add pagination
    So one chapter doesnt load all the ayah of surah

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function Home() {
     <ol>
       {versesData.surahsMetadata.map((surahMetadata, i) => (
         <li key={i}>
-          <Link to={`/chapters?chapter=${i + 1}`}>
+          <Link to={`/chapters?chapter=${i + 1}&page=1`}>
             {surahMetadata.nameTransliteration} ({surahMetadata.nameTranslation})
           </Link>
         </li>
--- src/ChaptersPage.tsx
+++ src/ChaptersPage.tsx
@@ -1,22 +1,51 @@
 import "./index.css"
-import { useSearchParams, Link } from 'react-router-dom';
+import { useSearchParams, Link } from 'react-router-dom'
 import * as versesData from './VersesData.ts'
 
 export function ChaptersPage() {
-  const [searchParams] = useSearchParams();
-  const chapter = searchParams.get('chapter');
-  const verses = versesData.getVerses(Number(chapter) - 1);
-
+  const [searchParams] = useSearchParams()
+  const chapter = searchParams.get('chapter')
+  const page = searchParams.get('page')
+  const verses = versesData.getVerses(Number(chapter) - 1)
   if (!verses) return <p>Not found</p>;
+
+  const versePerpageCount: number = 50
+  const verseCount: number = versesData.surahsMetadata[Number(chapter) - 1].versesCount
+  const surahNumber: number = versesData.surahsMetadata[Number(chapter) - 1].number
+  let pageCount : number = 1
+
+    if (verseCount > versePerpageCount) {
+        const quotient = verseCount / versePerpageCount
+        const modulo = verseCount % versePerpageCount
+        pageCount = quotient + Number(modulo != 0)
+    }
+  if ( Number(page) > pageCount) return <p>Not found</p>
+
+  let pageStartIndex : number = 0
+  let pageEndIndex : number = pageStartIndex + verseCount - 1 * Number(surahNumber == 1 || surahNumber == 9)
+
+    if (pageCount > 1) {
+      pageStartIndex = versePerpageCount * (Number(page) - 1)
+      if (Number(page) != pageCount) // if not in last page
+        pageEndIndex = pageStartIndex + versePerpageCount
+    }
+
+    const pages = []
+    for (let i = 1; i <= pageCount; i++) {
+        pages.push(<Link key={i} to={`?chapter=${chapter}&page=${i}`}>| {i} |</Link>)
+    }
+
     return (
         <>
             {<p>
                 <Link to={`/`}> Home </Link>
             </p>}
 
-            {verses.map((verse, index) => (
-              <p key={index} className="arabic-text">{verse.arabic}</p>
+            {verses.slice(pageStartIndex,pageEndIndex).map((verse, index) => (
+                <p key={index} className="arabic-text">{verse.arabic}</p>
             ))}
+
+            {<nav>{pages}</nav>}
         </>
     )
 }
