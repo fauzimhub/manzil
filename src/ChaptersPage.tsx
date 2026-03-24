50c63e529f889f4b27937ef1dde34000b26ca52e
fauzimhub <267496089+fauzimhub@users.noreply.github.com>
2026-03-24 20:14:47 +0700

    Refactor: Tidy up ChaptersPage
    Logic now easier to follow

--- src/ChaptersPage.tsx
+++ src/ChaptersPage.tsx
@@ -2,43 +2,25 @@ import "./index.css"
 import { useSearchParams, Link } from 'react-router-dom'
 import * as versesData from './VersesData.ts'
 
+
+const versePerpageCount: number = 50
+
 export function ChaptersPage() {
-  const [searchParams] = useSearchParams()
-  const chapter = searchParams.get('chapter')
-  const page = searchParams.get('page')
-  const verses = versesData.getVerses(Number(chapter) - 1)
-  if (!verses) return <p>Not found</p>;
+    const [searchParams] = useSearchParams()
+    const chapter = Number(searchParams.get('chapter'))
+    const page = Number(searchParams.get('page'))
 
-  const versePerpageCount: number = 50
-  const verseCount: number = versesData.surahsMetadata[Number(chapter) - 1].versesCount
-  const surahNumber: number = versesData.surahsMetadata[Number(chapter) - 1].number
-  let pageCount : number = 1
+    const verseCount = versesData.surahsMetadata[chapter - 1].versesCount
 
-    if (verseCount > versePerpageCount) {
-        const quotient = verseCount / versePerpageCount
-        const modulo = verseCount % versePerpageCount
-        pageCount = quotient + Number(modulo != 0)
-    }
-  if ( Number(page) > pageCount) return <p>Not found</p>
+    const verses = versesData.getVerses(chapter - 1)
+    if (!verses) return <p>Not found</p>;
 
-  let pageStartIndex : number = 0
-  let pageEndIndex : number = pageStartIndex + verseCount - 1 * Number(surahNumber == 1 || surahNumber == 9)
+    const pageCount = getPageCount(verseCount)
+    if ( page > pageCount) return <p>Not found</p>
 
-    if (pageCount > 1) {
-      pageStartIndex = versePerpageCount * (Number(page) - 1)
-      if (Number(page) != pageCount) // if not in last page
-        pageEndIndex = pageStartIndex + versePerpageCount
-    }
+    const pageIndex = getPageIndex(chapter,page,pageCount,verseCount)
+    const pageLinks = getPageLinks(chapter,pageCount)
 
-    const pages = []
-    for (let i = 1; i <= pageCount; i++) {
-        pages.push(<Link
-                      key={i}
-                      to={`?chapter=${chapter}&page=${i}`}
-                      onClick={() => window.scrollTo(0, 0)}>
-                    | {i} |
-                   </Link>)
-    }
 
     return (
         <>
@@ -46,11 +28,56 @@ export function ChaptersPage() {
                 <Link to={`/`}> Home </Link>
             </p>}
 
-            {verses.slice(pageStartIndex,pageEndIndex).map((verse, index) => (
+            {verses.slice(pageIndex.start,pageIndex.end).map((verse, index) => (
                 <p key={index} className="arabic-text">{verse.arabic}</p>
             ))}
 
-            {<nav>{pages}</nav>}
+            {<nav>{pageLinks}</nav>}
         </>
     )
+}
+
+
+const getPageCount =  (verseCount) => {
+    let count : number = 1
+
+    if (verseCount > versePerpageCount) {
+        const quotient = Math.floor(verseCount / versePerpageCount)
+        const modulo = verseCount % versePerpageCount
+        count = quotient + Number(modulo != 0)
+    }
+
+    return count
+}
+
+
+const getPageIndex = (chapter,page,pageCount,verseCount) => {
+  const surahNumber =  versesData.surahsMetadata[chapter - 1].number
+  let start : number = 0
+  let end : number = start + verseCount + 1 * Number(surahNumber != 1 || surahNumber != 9)
+
+    if (pageCount > 1) {
+        start = versePerpageCount * (page - 1)
+        if (page != pageCount) // if not in last page
+            end = start + versePerpageCount
+    }
+
+  return {
+    start : start,
+    end   : end,
+  }
+}
+
+const getPageLinks = (chapter,pageCount) => {
+    const links = []
+    for (let i = 1; i <= pageCount; i++) {
+        links.push(<Link
+                      key={i}
+                      to={`?chapter=${chapter}&page=${i}`}
+                      onClick={() => window.scrollTo(0, 0)}>
+                    | {i} |
+                   </Link>)
+    }
+
+    return links
 }
