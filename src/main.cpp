b08a5a58df617d5929593313233ce86d85c880e8
fauzim <fauzim@noreply.codeberg.org>
2026-06-23 08:42:14 +0700

    Checking nlohmann/json and WxWidgets

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#include <nlohmann/json.hpp>
 #include <wx/wx.h>
 
 class MyApp : public wxApp {
 public:
   bool OnInit() override {
+    nlohmann::json json = {{"name", "manzil"}, {"version", 1}};
+
     auto *frame =
-        new wxFrame(nullptr, wxID_ANY, "All your codebase are belong to us");
+        new wxFrame(nullptr, wxID_ANY, json["name"].get<std::string>());
     frame->Show();
     return true;
   }
