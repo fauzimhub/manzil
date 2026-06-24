1e270b3cc75284381d471777f1e809af09c125b6
fauzim <fauzim@noreply.codeberg.org>
2026-06-24 22:08:22 +0700

    Backtrack

--- src/manzil.cpp
+++ src/manzil.cpp
@@ -8,11 +8,12 @@
 using json = nlohmann::json;
 using ifstream = std::ifstream;
 using string = std::string;
+using std::cerr;
 using std::cout;
 
 bool manzil::App::OnInit() {
   json surah{};
-  string title = "no title";
+  string title = "Manzil";
   string surah_path = "assets/surah.json";
 
   ifstream file{surah_path};
@@ -20,56 +21,22 @@ bool manzil::App::OnInit() {
     try {
 
       surah = json::parse(file);
-      title = surah[0]["name_translation"].get<string>();
 
     } catch (std::exception& e) {
-      std::cerr << "<< Manzil: encountered an error while trying to parse \""
-                << surah_path << "\"" << " as json" << ": "
-                << "\n"
-                << e.what() << "\n";
+      cerr << "<< Manzil: encountered an error while trying to parse \""
+           << surah_path << "\"" << " as json" << ": "
+           << "\n"
+           << e.what() << "\n";
     }
   } else {
-    std::cerr << "<< Manzil: could not open \"" << surah_path << "\"\n";
+    cerr << "<< Manzil: could not open \"" << surah_path << "\"\n";
   }
 
   cout << surah << "\n";
 
-  auto* frame = new manzil::Frame(title);
+  auto* frame = new wxFrame(nullptr, wxID_ANY, title);
 
   frame->Show();
 
   return true;
-}
-
-BEGIN_EVENT_TABLE(manzil::Frame, wxFrame)
-EVT_MENU(wxID_ABOUT, manzil::Frame::OnAbout)
-EVT_MENU(wxID_EXIT, manzil::Frame::OnQuit)
-END_EVENT_TABLE()
-
-manzil::Frame::Frame(const wxString& title)
-    : wxFrame(nullptr, wxID_ANY, title) {
-
-  wxMenu* file_menu = new wxMenu;
-  wxMenu* help_menu = new wxMenu;
-
-  help_menu->Append(wxID_ABOUT, wxT("&About...\tF1"), wxT("Show about dialog"));
-  file_menu->Append(wxID_EXIT, wxT("E&xit\tAlt-X"), wxT("Quit this program"));
-  wxMenuBar* menu_bar = new wxMenuBar();
-  menu_bar->Append(file_menu, wxT("&File"));
-  menu_bar->Append(help_menu, wxT("&Help"));
-
-  SetMenuBar(menu_bar);
-
-  CreateStatusBar(2);
-  SetStatusText(wxT("Welcome to Manzil!"));
-}
-
-void manzil::Frame::OnAbout(wxCommandEvent& event) {
-  wxString msg;
-  msg.Printf(wxT("Hello and welcome to Manzil %s"), MANZIL_VERSION);
-  wxMessageBox(msg, wxT("About Minimal"), wxOK | wxICON_INFORMATION, this);
-}
-
-void manzil::Frame::OnQuit(wxCommandEvent& event) {
-  Close();
 }
--- src/manzil.hpp
+++ src/manzil.hpp
@@ -1,8 +1,6 @@
 #ifndef N_MANZIL_H_
 #define N_MANZIL_H_
 
-#include <wx/wx.h>
-
 namespace manzil {
 
 class App : public wxApp {
@@ -10,17 +8,5 @@ class App : public wxApp {
   bool OnInit() override;
 };
 
-class Frame : public wxFrame {
- public:
-  Frame(const wxString& title);
-
-  void OnQuit(wxCommandEvent& event);
-  void OnAbout(wxCommandEvent& event);
-
- private:
-  DECLARE_EVENT_TABLE();
-};
-
 }  // namespace manzil
-
 #endif  // N_MANZIL_H_
