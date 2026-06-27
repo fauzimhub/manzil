b2bcd4a1f20b2b3d6fc8ffe6d71d7d6ef23d35d7
fauzim <fauzim@noreply.codeberg.org>
2026-06-27 23:58:32 +0700

    Feat: Add surah_card component

---
+++ src/components/surah_card.cpp
@@ -0,0 +1,104 @@
+#include <wx/wx.h>
+
+#include <unordered_map>
+#include "surah_card.hpp"
+
+using font_map = std::unordered_map<wxString, wxFont>;
+using std::move;
+
+SurahCard::SurahCard(wxWindow* parent, wxString number, wxString arabic,
+                     wxString transliteration, wxString translation,
+                     wxString verses)
+    : wxPanel(parent, wxID_ANY),
+      number_(move(number)),
+      arabic_(move(arabic)),
+      transliteration_(move(transliteration)),
+      translation_(move(translation)),
+      verses_(move(verses)) {
+
+  UpdateMinSize();
+
+  Bind(wxEVT_PAINT, &SurahCard::OnPaint, this);
+}
+
+void SurahCard::OnPaint(wxPaintEvent& event) {
+  (void)event;
+  wxPaintDC device_contexts(this);
+  const wxSize size = GetClientSize();
+  const int surahcard_w = size.GetWidth();
+  const int surahcard_h = size.GetHeight();
+
+  device_contexts.SetBackground(wxBrush(GetParent()->GetBackgroundColour()));
+  device_contexts.Clear();
+
+  const wxColour white(255, 255, 255);
+  const wxColour gray(200, 200, 200);
+  const wxColour black(0, 0, 0);
+  constexpr int pen_width = 1;
+  constexpr wxCoord rect_x = 0;
+  constexpr wxCoord rect_y = 0;
+  constexpr double rect_radius = 10;
+
+  device_contexts.SetPen(wxPen(white, pen_width));
+  device_contexts.SetBrush(wxBrush(white));
+  device_contexts.SetTextForeground(black);
+
+  device_contexts.DrawRoundedRectangle(rect_x, rect_y, surahcard_w, surahcard_h,
+                                       rect_radius);
+
+  wxCoord text_y = padding_;
+
+  auto centerpad_text = [&](const wxString& text) {
+    wxSize ext = device_contexts.GetTextExtent(text);
+    device_contexts.DrawText(text, (surahcard_w - ext.GetWidth()) / 2, text_y);
+    text_y += ext.GetHeight() + padding_;
+  };
+
+  device_contexts.SetFont(wxFont(FromDIP(number_fontsize_),
+                                 wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL,
+                                 wxFONTWEIGHT_BOLD));
+  centerpad_text(number_);
+
+  device_contexts.SetFont(wxFont(FromDIP(name_fontsize_), wxFONTFAMILY_DEFAULT,
+                                 wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+  centerpad_text(transliteration_ + " | " + arabic_);
+
+  device_contexts.SetFont(wxFont(FromDIP(translation_fontsize_),
+                                 wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL,
+                                 wxFONTWEIGHT_BOLD));
+  centerpad_text(translation_);
+
+  const wxString verses_str = verses_ + " verses";
+  device_contexts.SetFont(wxFont(FromDIP(verses_fontsize_),
+                                 wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL,
+                                 wxFONTWEIGHT_NORMAL));
+  wxSize vext = device_contexts.GetTextExtent(verses_str);
+  const wxCoord verses_x = (surahcard_w - vext.GetWidth()) / 2;
+  const wxCoord verses_y = text_y;
+  device_contexts.SetPen(wxPen(gray, pen_width));
+  device_contexts.DrawRoundedRectangle(
+      verses_x, verses_y, vext.GetWidth() + padding_,
+      vext.GetHeight() + padding_, rect_radius / 2);
+  device_contexts.DrawText(verses_str, verses_x + (padding_ / 2),
+                           verses_y + (padding_ / 2));
+}
+
+void SurahCard::UpdateMinSize() {
+  wxClientDC device_contexts(this);
+  wxCoord total_height = padding_;
+
+  auto measure = [&](int font_size, wxFontWeight weight, const wxString& text) {
+    device_contexts.SetFont(wxFont(FromDIP(font_size), wxFONTFAMILY_DEFAULT,
+                                   wxFONTSTYLE_NORMAL, weight));
+    total_height += device_contexts.GetTextExtent(text).GetHeight() + padding_;
+  };
+
+  measure(number_fontsize_, wxFONTWEIGHT_BOLD, number_);
+  measure(name_fontsize_, wxFONTWEIGHT_NORMAL,
+          transliteration_ + " | " + arabic_);
+  measure(translation_fontsize_, wxFONTWEIGHT_BOLD, translation_);
+  measure(verses_fontsize_, wxFONTWEIGHT_NORMAL, verses_ + " verses");
+  total_height += padding_;
+
+  SetMinSize(wxSize(wxDefaultCoord, total_height));
+}
---
+++ src/components/surah_card.hpp
@@ -0,0 +1,28 @@
+#ifndef N_SURAH_CARD_H_
+#define N_SURAH_CARD_H_
+
+#include <wx/wx.h>
+#include <unordered_map>
+
+using font_map = std::unordered_map<wxString, wxFont>;
+
+class SurahCard : public wxPanel {
+ public:
+  SurahCard(wxWindow* parent, wxString number, wxString arabic,
+            wxString transliteration, wxString translation, wxString verses);
+  void OnPaint(wxPaintEvent& event);
+
+ private:
+  void UpdateMinSize();
+  static constexpr int number_fontsize_ = 14;
+  static constexpr int name_fontsize_ = 11;
+  static constexpr int translation_fontsize_ = 10;
+  static constexpr int verses_fontsize_ = 8;
+  static constexpr wxCoord padding_ = 8;
+  wxString number_;
+  wxString arabic_;
+  wxString transliteration_;
+  wxString translation_;
+  wxString verses_;
+};
+#endif  // N_SURAH_CARD_H_
