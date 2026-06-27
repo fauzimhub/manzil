826ef930c98bb1d5ea50665a0b67db4e1794a5d2
fauzim <fauzim@noreply.codeberg.org>
2026-06-27 23:56:00 +0700

    Fix: Clean up get surah check in manzil.cpp
    moved to frame.cpp on next commit

--- src/manzil.cpp
+++ src/manzil.cpp
@@ -1,27 +1,13 @@
 #include <wx/wx.h>
-#include <iostream>
 #include <string>
 
 #include "components/frame.hpp"
 #include "manzil.hpp"
-#include "quranite.hpp"
 
 using string = std::string;
-using std::cout;
 
 bool manzil::App::OnInit() {
   string title = "Manzil";
-  string surah_path = "assets/surah.json";
-
-  auto* quranite = new manzil::Quranite(surah_path);
-
-  for (const auto& sur : quranite->getSurah()) {
-    cout << "Surah (Number: " << sur.number
-         << ", Name Arabic: " << sur.name_arabic
-         << ", Name Translation: " << sur.name_translation
-         << ", Name Transliteration: " << sur.name_transliteration
-         << ", Verses Count: " << sur.verses_count << ")" << "\n";
-  }
 
   auto* frame = new Frame(title);
 
