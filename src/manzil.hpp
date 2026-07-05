f6a91b157b1d3c3db13ccb37e0c416ef759053d2
fauzim <fauzim@noreply.codeberg.org>
2026-07-05 10:12:37 +0700

    Fix: manzil.hpp change define guard

--- src/manzil.hpp
+++ src/manzil.hpp
@@ -1,5 +1,26 @@
-#ifndef N_MANZIL_H_
-#define N_MANZIL_H_
+/* manzil.hpp: bootstrap manzil.
+
+Copyright (c) 2026 Maher Fauzi 
+   Permission is hereby granted, free of charge, to any person obtaining a copy of
+   this software and associated documentation files (the "Software"), to deal in
+   the Software without restriction, including without limitation the rights to
+   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+   of the Software, and to permit persons to whom the Software is furnished to do
+   so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+
+*/
+
+#ifndef MANZIL_HPP
+#define MANZIL_HPP
 
 #include <wx/wx.h>
 
@@ -11,4 +32,4 @@ class App : public wxApp {
 };
 
 }  // namespace manzil
-#endif  // N_MANZIL_H_
+#endif  // MANZIL_HPP
