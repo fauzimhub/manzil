eb68f148ef391004d13732a438c288925396d248
fauzim <fauzim@noreply.codeberg.org>
2026-07-06 09:33:04 +0700

    Fix: Delete custom event NoteClickedEvent

--- src/events.cpp
+++ src/events.cpp
@@ -1,5 +1,3 @@
 #include "events.hpp"
 
-// NOLINTNEXTLINE(bugprone-throwing-static-initialization)
 wxDEFINE_EVENT(EVT_SURAH_SELECTED, wxCommandEvent);
-wxDEFINE_EVENT(EVT_NOTE_CLICKED, NoteClickedEvent);
