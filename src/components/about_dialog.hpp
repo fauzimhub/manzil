72940c513b19e026993448b61cc09b936b7a61c9
fauzim <fauzim@noreply.codeberg.org>
2026-07-07 10:35:40 +0700

    Docs: Add comments and license in about_dialog.hpp

--- src/components/about_dialog.hpp
+++ src/components/about_dialog.hpp
@@ -1,11 +1,35 @@
-#ifndef N_ABOUT_DIALOG_H_
-#define N_ABOUT_DIALOG_H_
+/*
+Copyright (c) 2026 Maher Fauzi 
+   Permission is hereby granted, free of charge, to any person obtaining a copy of
+   this software and associated documentation files (the "Software"), to deal in
+   the Software without restriction, including without limitation the rights to
+   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+   of the Software, and to permit persons to whom the Software is furnished to do
+   so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+#ifndef ABOUT_DIALOG_HPP
+#define ABOUT_DIALOG_HPP
 
 #include <wx/wx.h>
 
+/**
+ * @brief custom dialog for manzil help menu.
+ *
+ * Contain link to project and link to official quranite reader
+ *
+ */
 class AboutDialog : public wxDialog {
  public:
   explicit AboutDialog(wxWindow* parent);
 };
 
-#endif  // N_ABOUT_DIALOG_H_
+#endif  // ABOUT_DIALOG_HPP
